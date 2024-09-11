f8d46f57fbd78284aa4ea1663e502368f02f3102
kukmeister <kacpersiudowski@gmail.com>
2024-09-11 11:31:19 +0200

    fixed for location, and world also addes quest cs

---
+++ ProjectAlpha/Template for code game/Quest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Quest
+{
+    public int ID;
+    public string Name;
+    public string Description;
+
+    public Quest(int id, string name, string description)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+
+    }
+}
