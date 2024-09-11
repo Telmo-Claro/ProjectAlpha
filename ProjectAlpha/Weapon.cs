73fc59db4405de2ca86bdb34ee21ede9a2f98044
Telmo-Claro <telmoclaro@proton.me>
2024-09-11 11:28:56 +0200

    constructor added

--- ProjectAlpha/Weapon.cs
+++ ProjectAlpha/Weapon.cs
@@ -8,4 +8,11 @@ public class Weapon
     public int id;
     public int maximumDamage;
     public string name;
+
+    public Weapon(int id, int maximumDamage, string name)
+    {
+        this.id = id;
+        this.maximumDamage = maximumDamage;
+        this.name = name;
+    }
 }
