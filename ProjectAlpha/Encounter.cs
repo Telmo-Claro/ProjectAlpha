79d8bb700309d248916def0632a6bc876005b50e
Telmo-Claro <telmoclaro@proton.me>
2024-09-13 14:06:28 +0200

    Monster encounter added

---
+++ ProjectAlpha/Encounter.cs
@@ -0,0 +1,36 @@
+﻿public class Encounter
+    {
+
+    public Player mc;
+    public Monster monsterLivingHere;
+
+    public Encounter(Player mc, Monster monsterLivingHere)
+    {
+        this.mc = mc;
+        this.monsterLivingHere = monsterLivingHere;
+    }
+
+    public string choice()
+    {
+        while (true)
+        {
+            Console.WriteLine($"{mc.Name} is facing {monsterLivingHere.Name}");
+            Console.WriteLine("How do you wish to proceed:");
+            Console.Write("1) Attack\n2) Sneak\n3) Flee\n> ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                return "Attack";
+            }
+            else if (choice == 2)
+            {
+                return "Sneak";
+            }
+            else if (choice == 3)
+            {
+                return "Flee";
+            }
+
+        }
+    }
+    }
