dfcdd66e83293ac5549025db78351b1dfd57f7aa
Axel <1059015@hr.nl>
2024-09-17 11:31:39 +0200

    monster ehp aangepast

--- ProjectAlpha/World.cs
+++ ProjectAlpha/World.cs
@@ -55,13 +55,13 @@ public static class World
 
     public static void PopulateMonsters()
     {
-        Monster rat = new Monster(MONSTER_ID_RAT, "rat", 15, 25, 10);
+        Monster rat = new Monster(MONSTER_ID_RAT, "rat", 15, 25, 25);
 
 
         Monster snake = new Monster(MONSTER_ID_SNAKE, "snake", 10, 7, 7);
 
 
-        Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "giant spider", 3, 10, 10);
+        Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "giant spider", 3, 200, 200);
 
 
         Monsters.Add(rat);
